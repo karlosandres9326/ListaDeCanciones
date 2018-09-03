f71bebd26e9afacd121d6ef220822d8edd0d1890
jonmid <jonmid@gmail.com>
2018-09-03 17:44:32 -0500

    first commit

---
+++ resources/views/plantilla/artistas.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista de ##### </title>
+</head>
+<body>
+    
+    <center>
+    <table border=1>
+        <tr>
+            <td colspan=2><center><b> ##### </b</center></td>
+        </tr>
+
+        
+        <tr>
+            <td><b> ##### </b></td>
+            <td> ##### </td>
+        </tr>
+        
+
+        <tr>
+            <td colspan=2><center><b> ##### </b</center></td>
+        </tr>
+    </table>
+    </center>
+    
+</body>
+</html>
